6def0596dc23cda2d482be601bbaa45c0ac97cb1
ElMatoi <91705636+ElMatoi@users.noreply.github.com>
2024-07-23 00:58:28 -0400

    fix Dockerfiles, agregado el graphql en la gateway

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,15 +1,19 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphqlModule } from './graphql.module'; 
+import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/default'),
+    GraphqlModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver], 
 })
 export class AppModule {}
